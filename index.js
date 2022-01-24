5f3f98506d217475325f083fecbdaba63805048a
alex-beketov <alex.pro0075@yandex.ru>
2022-01-24 21:09:44 +0300

    added an oportunity to count time in the usual mode

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ function countTime(status, sender, sendResponse) {
 		let hours = 0;
 		let minutes = 0;
 		let seconds = 0;
-		const times = document.querySelectorAll('.style-scope ytd-thumbnail-overlay-time-status-renderer');
+		if (document.querySelectorAll('ytd-playlist-video-renderer').length > 0){
+			const times = document.querySelectorAll('ytd-playlist-video-renderer > .style-scope ytd-thumbnail-overlay-time-status-renderer');
+			for (let i = 0; i < times.length; i++) {
+				let time = times[i].innerText.split(':');
+				if (time.length === 3) {
+					hours += +time[0];
+					minutes += +time[1];
+					seconds += +time[2];
+				} else {
+					minutes += +time[0];
+					seconds += +time[1];
+				}
+			}
+			minutes += Math.trunc(seconds / 60);
+			seconds = seconds % 60;
+			hours += Math.trunc(minutes / 60);
+			minutes = minutes % 60;
+			return alert(`hours: ${hours} minutes: ${minutes} seconds: ${seconds}`)
+		} else {
+		const times = document.querySelectorAll('#content ytd-playlist-panel-video-renderer > .style-scope ytd-thumbnail-overlay-time-status-renderer');
 		for (let i = 0; i < times.length; i++) {
 			let time = times[i].innerText.split(':');
 			if (time.length === 3) {
@@ -19,7 +38,8 @@ function countTime(status, sender, sendResponse) {
 		seconds = seconds % 60;
 		hours += Math.trunc(minutes / 60);
 		minutes = minutes % 60;
-		alert(`hours: ${hours} minutes: ${minutes} seconds: ${seconds}`)
+		return alert(`hours: ${hours} minutes: ${minutes} seconds: ${seconds}`)
+		}
 	}
 }
 
