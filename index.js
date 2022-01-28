6e78125f55794f094e9c725e94b50f9b8202a4ee
alex-beketov <alex.pro0075@yandex.ru>
2022-01-28 18:20:42 +0300

    fixed some bags

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-function countTime(status, sender, sendResponse) {
-	if(status){
+const example = (status, sender, sendResponse) => {
+	if(status) {
+		countTime()
+	}
+}
+function countTime() {
 		let hours = 0;
 		let minutes = 0;
 		let seconds = 0;
-		if (document.querySelectorAll('ytd-playlist-video-renderer').length > 0){
+		if (document.querySelectorAll('#content ytd-playlist-video-renderer').length > 0 && document.querySelector('#content ytd-playlist-video-renderer').offsetHeight !== 0){
 			const times = document.querySelectorAll('ytd-playlist-video-renderer > .style-scope ytd-thumbnail-overlay-time-status-renderer');
 			for (let i = 0; i < times.length; i++) {
 				let time = times[i].innerText.split(':');
@@ -20,8 +24,9 @@ function countTime(status, sender, sendResponse) {
 			seconds = seconds % 60;
 			hours += Math.trunc(minutes / 60);
 			minutes = minutes % 60;
-			return alert(`hours: ${hours} minutes: ${minutes} seconds: ${seconds}`)
-		} else {
+			return alert(`${hours}:${minutes}:${seconds}`)
+		} else if (document.querySelectorAll('#content ytd-playlist-panel-video-renderer').length > 0 
+		&& document.querySelector('#content ytd-playlist-panel-video-renderer') !== 0){
 		const times = document.querySelectorAll('#content ytd-playlist-panel-video-renderer > .style-scope ytd-thumbnail-overlay-time-status-renderer');
 		for (let i = 0; i < times.length; i++) {
 			let time = times[i].innerText.split(':');
@@ -34,13 +39,12 @@ function countTime(status, sender, sendResponse) {
 				seconds += +time[1];
 			}
 		}
-		minutes += Math.trunc(seconds / 60);
-		seconds = seconds % 60;
-		hours += Math.trunc(minutes / 60);
-		minutes = minutes % 60;
-		return alert(`hours: ${hours} minutes: ${minutes} seconds: ${seconds}`)
-		}
+			minutes += Math.trunc(seconds / 60);
+			seconds = seconds % 60;
+			hours += Math.trunc(minutes / 60);
+			minutes = minutes % 60;
+			return alert(`${hours}:${minutes}:${seconds}`)
 	}
 }
 
-chrome.runtime.onMessage.addListener(countTime)
+chrome.runtime.onMessage.addListener(example)
